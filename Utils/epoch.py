054a37ba67cc2f4aab7fe6a10a0ef16bebe674ba
ImMoa <yumemiayumu@gmail.com>
2023-10-02 17:16:11 +0900

    Fix somethings
    - Fix timestamp converter
    
    - Fix maint parser error can be caused in certain situation

--- Utils/epoch.py
+++ Utils/epoch.py
@@ -10,14 +10,10 @@ class Epoch:
     def ConvertTime(year, month, day, hour, min, sec):
         try:
             # 입력된 날짜와 시간을 datetime 객체로 생성하고 타임존을 직접 설정
-            input_time = datetime(year, month, day, hour, min, sec, tzinfo=timezone('UTC'))
-
-            # 입력된 시간을 GMT+9로 변환 (일본 시간대)
-            jst_timezone = timezone('Asia/Tokyo')
-            jst_time = input_time.astimezone(jst_timezone)
+            input_time = timezone('Asia/Tokyo').localize(datetime(year, month, day, hour, min, sec))
 
             # JST 시간을 타임스탬프로 변환
-            timestamp = trunc(int(jst_time.timestamp()))
+            timestamp = trunc(int(input_time.timestamp()))
 
         except Exception as e:
             lg.error(f"Something went wrong while processing ConvertTime()!!\nError: {e}")
@@ -28,7 +24,7 @@ class Epoch:
     def ConvertStamp(stamp):
 
         try:
-            detTime = datetime.fromtimestamp(stamp, timezone('Asia/Seoul'))
+            detTime = datetime.fromtimestamp(stamp, timezone('Asia/Tokyo'))
             detTime = detTime.strftime('%Y-%m-%d %H:%M:%S')
 
         except Exception as e:
--- chariot.py
+++ chariot.py
@@ -415,6 +415,8 @@ async def deepl(interaction: discord.Interaction, query: str, src: str=None, dst
 async def maintinfo(interaction: discord.Interaction):
     """공지 관련 정보를 임베드로 작성"""
 
+    await interaction.response.defer(ephemeral=False)
+
     lg.info(f"{interaction.user.display_name} request maintinfo()")
     
     # 0 = Start, 1 = End, 2 = Title, 3 = Link
@@ -426,7 +428,7 @@ async def maintinfo(interaction: discord.Interaction):
                               timestamp=datetime.datetime.utcnow())
         embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1138398345065414657/1138398369929244713/0001061.png")
         embed.add_field(name="", value="현재 확인할 수 있는 점검 공지가 없습니다.\n무언가 문제가 있다면 공식 로드스톤을 참고해 주세요.")
-        await interaction.response.send_message(embed=embed)
+        await interaction.followup.send(embed=embed)
         return
 
     output = Translator.Translate(time_info[2])
@@ -438,7 +440,7 @@ async def maintinfo(interaction: discord.Interaction):
                           timestamp=datetime.datetime.utcnow())
     embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1138398345065414657/1138398369929244713/0001061.png")
     embed.add_field(name="일정", value=f'시작 : <t:{time_info[0]}:F> \n종료 : <t:{time_info[1]}:F> \n\n<t:{time_info[1]}:R>', inline=False)
-    await interaction.response.send_message(embed=embed)
+    await interaction.followup.send(embed=embed)
 
     lg.info(f'공지 링크 : {time_info[3]}')
 #endregion
