74d44b563934c182122012169687accab8a0a8dc
ImMoa <yumemiayumu@gmail.com>
2024-01-17 02:19:36 +0900

    Update kill_bot.sh

--- kill_bot.sh
+++ kill_bot.sh
@@ -4,12 +4,18 @@
 process_name="python3 chariot.py"
 
 # 프로세스를 찾아서 PID를 얻음
-pid=$(pgrep -f "$process_name")
+pids=$(pgrep -f "$process_name")
 
 # PID가 존재하는지 확인하고 프로세스를 종료
-if [ -n "$pid" ]; then
-    echo "프로세스를 찾았습니다. PID: $pid"
-    kill "$pid"
+if [ -n "$pids" ]; then
+    echo "프로세스를 찾았습니다. PIDs: $pids"
+    
+    # 모든 PID에 대해 반복하여 종료
+    for pid in $pids; do
+        echo "프로세스를 종료합니다. PID: $pid"
+        kill "$pid"
+    done
+    
     echo "프로세스를 종료했습니다."
 else
     echo "프로세스를 찾을 수 없습니다."
